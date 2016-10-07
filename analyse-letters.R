a77ad52ef84ef08909663de4449f0fc9c4ee1e1f
SaraJKerr <sarajkerr@me.com>
2016-10-07 23:31:12 +0100

    Initial commit

---
+++ analyse-letters.R
@@ -0,0 +1,58 @@
+################################################################################
+# File-Name: analyse-letters.R                                                 #
+# Date: 7 October 2016                                                         #
+# Author: Sara J Kerr                                                          # 
+# ORCID:orcid.org/0000-0002-2322-1178                                          #
+# Institution: Maynooth University                                             #
+# Project: Letters of 1916 - Prof Susan Schreibman                             #
+# Purpose: workflow script for analysing letters                               #
+# Based on: http://www.informit.com/articles/article.aspx?p=2215520            #
+#       Jockers (2014) Text Analysis with R for Students of Literature         #
+# Data Used: Letters in xml format                                             #
+# Packages Used: XML, plyr,                                                    #
+# Input:                                                                       #
+# Output:                                                                      #
+# Last Updated: 7 October 2016                                                 #
+################################################################################
+
+# Load packages
+require("XML")
+require("plyr")
+
+# Set working directory using Session option in R Studio
+
+# Source functions to be used
+
+source("Code/Letters_1916_Internship/extract-data.R")
+
+# Identify the folder where the data is stored
+input_dir <- "RawData/Letters"
+
+# Create a vector with the contents of the directory
+files <- dir(input_dir, "\\.xml$")
+
+# Create a vector with the full filepath
+# Problematic files have been removed - to be dealt with later
+x <- file.path(input_dir,files) 
+
+
+# Create empty lists
+letter_data <- list()
+keywords <- list()
+words <- list()
+
+# Loop over the files extracting the information
+for (i in 1:length(x)) {
+        xmlfile <- xmlParse(x[i], useInternalNodes = T)  
+        results <- extract_data(xmlfile)
+        letter_data <- c(letter_data, results[1])
+        keywords <- c(keywords, results[2])
+        words <- c(words, results[3])
+}
+
+# Convert lists into data frames
+letter_data <- ldply(letter_data, data.frame)
+keywords <- ldply(keywords, data.frame)
+words <- ldply(words, data.frame)
+
+
